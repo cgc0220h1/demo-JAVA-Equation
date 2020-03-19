9128cd53b90cbf11be017981b9709beec677bfa3
vanduc2514 <vanduc2514@gmail.com>
2020-03-19 14:30:49 +0700

    Refactor

--- src/Equation.java
+++ src/Equation.java
@@ -2,31 +2,18 @@ import java.util.Scanner;
 
 public class Equation {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        {
-            System.out.println("Giải phương trình bậc 1");
-            System.out.println("Nhập vào các hằng số của phương trình 'a * x + b = c'");
-        }
-
         double a;
-        {
-            System.out.println("Nhập vào số a: ");
-            a = scanner.nextDouble();
-        }
-
         double b;
-        {
-            System.out.println("Nhập vào số b: ");
-            b = scanner.nextDouble();
-        }
-
         double c;
-        {
-            System.out.println("Nhập vào số c: ");
-            c = scanner.nextDouble();
-        }
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Giải phương trình bậc 1");
+        System.out.println("Nhập vào các hằng số của phương trình 'a * x + b = c'");
+        System.out.println("Nhập vào số a: ");
+        a = scanner.nextDouble();
+        System.out.println("Nhập vào số b: ");
+        b = scanner.nextDouble();
+        System.out.println("Nhập vào số c: ");
+        c = scanner.nextDouble();
         if (a != 0) {
             double solution = (c - b) / a;
             System.out.printf("Nghiệm của phương trình là: %f!\n", solution);
